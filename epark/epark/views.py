da630feb1402f20416478e7036e75d8bf49c78e7
GermanCarchi <germancarchi@gmail.com>
2022-08-15 08:42:53 -0500

    Registro y Login de administrador

--- epark/epark/views.py
+++ epark/epark/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import login
 # genera la sesion
 from django.contrib.auth import authenticate
 
-from .forms import RegisterForm
+from .forms import RegisterForm,RegisterFormOwner
 # para autenticar usuarios
 
 from .forms import RegisterForm
@@ -48,11 +48,36 @@ def login_view(request):
             messages.error(request, "Usuario o Contraseña Incorrectos.")
             # se debe colocar en el template si hay mensajes para visualizarlos
             
-
-
-
     return render(request, 'users/login.html', {
+    })
 
+def login_owner_view(request):
+    # request.method es POST cuando se toca el boton enviar en el login
+    # si se presiono el boton, obtiene el input name username y password 
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')        
+        
+        # busca en la base de datos un usuario que tenga ese username y contranseña
+        # si existe nos retorna un objeto User almacenable si no existe nos retorna NONE
+        userOwner = authenticate(username=username, password=password)
+        
+        if userOwner: 
+            # para generar una sesion necesita peticion (request) y el usuario (user)
+            login(request, userOwner)
+            # Mensaje de login correcto 
+            messages.success(request, "Bienvenido {}".format(username))
+            # se debe colocar en el template si hay mensajes para visualizarlos
+            
+            # concretada lo anterior redirije al string que se coloca dentro. 
+            return redirect('index')
+        # Si user retorna NONE
+        else:
+            messages.error(request, "Usuario o Contraseña Incorrectos.")
+            # se debe colocar en el template si hay mensajes para visualizarlos
+            
+    return render(request, 'users/login_owner.html', {
     })
 
 def logout_view(request):
@@ -75,8 +100,6 @@ def register_view(request):
         email = form.cleaned_data.get('email')
         password = form.cleaned_data.get('password')
         """
-        
-
         # crear nuevo usuario 
         user = form.save() # retorna objeto user
         if user:
@@ -87,6 +110,32 @@ def register_view(request):
 
     return render(request, 'users/register.html', {
         'form': form
+    })
+
+def register_owner_view(request):
+    # Trae el formulario de administrador/dueño y se debe agregar al contexto
+    formOwner = RegisterFormOwner(request.POST or None)    
+    # Si el metodo es post se genera un formulario con los datos del usuario
+    # Si la peticion no es POST es decir None el formulario es vacio
+
+    # is_valid nos indica si el formulario es valido
+    if request.method == 'POST' and formOwner.is_valid():
+        #si es valido se obtienen los datos
+        """ SE OPTIMIZA ESTE CODIGO POR FORMS.SAVE
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        """
+        # crear nuevo usuario 
+        userOwner = formOwner.save() # retorna objeto user
+        if userOwner:
+            login(request,userOwner)
+            messages.success(request, 'Usuario creado exitosamente')
+            return redirect('index')
+
+
+    return render(request, 'users/register_owner.html', {
+        'formOwner': formOwner
     })
 
 def parking_view(request):
